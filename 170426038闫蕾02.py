29deb76ce219e3901fb8742e01c1d3de66e9fc8e
yanlei23 <51936773+yanlei23@users.noreply.github.com>
2019-06-20 17:27:57 +0800

    Delete 170426038闫蕾02.py

--- 170426038闫蕾02.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-url='http://www.taobao.com'
-proxy_handle={'http':'http://222.95.16.235:9999'
-              'http':'http://60.205.57.4:9999' 
- }
-proxy=urllib.request.ProxyHandler(proxy_handle)
-opener=urllib.request.build_opener(proxy)
-response=opener.open(url)
-print(response.read()，decode('utf8'))
